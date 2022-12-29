8849325348b8b9a9092b96130ee27289a2a6c136
mojadev <mojadev@gmail.com>
2022-12-29 15:23:39 +0100

    chore: add linter script

--- src/generator/koa/schema.ts
+++ src/generator/koa/schema.ts
@@ -5,11 +5,18 @@ import { Writer } from '../writer';
 import { RoutingModel } from '../../model';
 import { getModelType } from '../../model/code-model.decorator';
 
+const fileRules = [
+  '/* eslint-disable @typescript-eslint/array-type */',
+  '/* eslint-disable @typescript-eslint/no-empty-interface */',
+  '/* eslint-disable @typescript-eslint/no-explicit-any */',
+];
+
 export class SchemaFileGenerator implements CodeGenerator<RoutingModel> {
   constructor(private readonly registry: Registry) {}
 
   generate(model: RoutingModel, writer: Writer = new Writer()): Writer {
-    writer.writeLine('/* eslint-disable @typescript-eslint/array-type */');
+    fileRules.forEach(writer.writeLine);
+
     Object.entries(model.types)
       .map(([key, typeModel]) => generateCodeModelForType(key, typeModel))
       .flatMap((model) => [...identifyImports(model), model])
