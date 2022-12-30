a547bf3447dfa1bcaf70166bc86b7141b277a5e8
mojadev <mojadev@gmail.com>
2022-12-30 18:12:14 +0100

    feat: add example for koa-zod-errorhandler  (refs: 19)
    chore: remove unused code
    fix: graceful shutdown of server

--- e2e/echo-server/server.ts
+++ e2e/echo-server/server.ts
@@ -1,12 +1,13 @@
 import Koa from 'koa';
 import { registry, router } from './router';
+import { zodErrorHandler } from 'koa-zod-errorhandler';
 import cors from '@koa/cors';
 
 const port = process.argv[2] ?? 8081;
 const app = new Koa();
 
 app.use(cors());
-
+app.use(zodErrorHandler());
 app.use(router.routes());
 
 registry.register('echoRequest', 'application/json', (args) => {
